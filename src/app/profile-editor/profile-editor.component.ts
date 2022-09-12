d8dec02978a09659d6be07f1744e5c09d00d8294
zophrox <zophrox@gmail.com>
2022-09-12 21:12:09 +0300

    without FormBuilder

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,54 +1,42 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder,  Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
   templateUrl: './profile-editor.component.html',
-  styleUrls: ['./profile-editor.component.scss']
+  styleUrls: ['./profile-editor.component.scss'],
 })
 export class ProfileEditorComponent {
-constructor(private fb:FormBuilder){}
-//  profileForm = new FormGroup({
-//    firstName: new FormControl(''),
-//    lastName: new FormControl(''),
-//    address: new FormGroup({
-//      street: new FormControl(''),
-//      city: new FormControl(''),
-//      state: new FormControl(''),
-//      zip: new FormControl('')
-//    })
-//  })
-
-profileForm = this.fb.group({
-  firstName: ['',Validators.required],
-  lastName: [''],
-  address: this.fb.group({
-    street: [''],
-    city: [''],
-    state: [''],
-    zip: ['']
-  }),aliases: this.fb.array([
-    this.fb.control('')
-  ])
-})
-get aliases() {
-  return this.profileForm.get('aliases') as FormArray;
-}
-addAlias() {
-  this.aliases.push(this.fb.control(''));
-}
- onSubmit(){
-    console.warn(this.profileForm.value);
-    // alert(this.profileForm.valid)
- }
- updateProfile() {
-  this.profileForm.patchValue({
-    firstName: 'Oleg',
-    address: {
-      street: '123 Drew Street'
-    }
+  // constructor(private fb:FormBuilder){}
+  profileForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl(''),
+    }),
+    aliases: new FormArray([new FormControl('')]),
   });
-}
 
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
 
+  addAlias() {
+    const aliases = new FormControl(null, Validators.required);
+    (<FormArray>this.profileForm.get('aliases')).push(aliases);
+  }
+  onSubmit() {
+    console.warn(this.profileForm.value);
+  }
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Oleg',
+      address: {
+        street: '123 Drew Street',
+      },
+    });
+  }
 }
