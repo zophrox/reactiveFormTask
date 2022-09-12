ccdb7aebab0acaba291d0c873640a264110bc019
zophrox <zophrox@gmail.com>
2022-09-12 20:26:14 +0300

    finish

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder,  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -7,14 +7,48 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./profile-editor.component.scss']
 })
 export class ProfileEditorComponent {
+constructor(private fb:FormBuilder){}
+//  profileForm = new FormGroup({
+//    firstName: new FormControl(''),
+//    lastName: new FormControl(''),
+//    address: new FormGroup({
+//      street: new FormControl(''),
+//      city: new FormControl(''),
+//      state: new FormControl(''),
+//      zip: new FormControl('')
+//    })
+//  })
 
- profileForm = new FormGroup({
-   firstName: new FormControl(''),
-   lastName: new FormControl('')
- })
+profileForm = this.fb.group({
+  firstName: ['',Validators.required],
+  lastName: [''],
+  address: this.fb.group({
+    street: [''],
+    city: [''],
+    state: [''],
+    zip: ['']
+  }),aliases: this.fb.array([
+    this.fb.control('')
+  ])
+})
+get aliases() {
+  return this.profileForm.get('aliases') as FormArray;
+}
+addAlias() {
+  this.aliases.push(this.fb.control(''));
+}
  onSubmit(){
-   
-   console.warn(this.profileForm.value);
+    console.warn(this.profileForm.value);
+    // alert(this.profileForm.valid)
  }
+ updateProfile() {
+  this.profileForm.patchValue({
+    firstName: 'Oleg',
+    address: {
+      street: '123 Drew Street'
+    }
+  });
+}
+
 
 }
